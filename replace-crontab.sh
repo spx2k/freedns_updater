7177cf90fbeaaefab939a2d1785eac927f2829da
spx2k <120874553+spx2k@users.noreply.github.com>
2024-02-11 13:15:19 +0100

    Update replace-crontab.sh
    add minutes interval to config

--- replace-crontab.sh
+++ replace-crontab.sh
@@ -6,17 +6,27 @@ if [ -e "$JOB_FILE" ]; then
     echo "Job already exists, not first run."
     exit 0
 fi
-
-if [ -z "$HOURS_DELAY" ]; then
-    echo "HOURS_DELAY not set, defaulting to 6 hours."
-    HOURS_DELAY="*/6"
-elif (( HOURS_DELAY < 24 )) && (( HOURS_DELAY > 0 )); then
-    echo "Adding job to run every ${HOURS_DELAY} hours."
-    HOURS_DELAY="*/${HOURS_DELAY}"
+if [ -z "$MINS_INTERVAL" ]; then
+    echo "MINS_INTERVAL not set, defaulting to 20 minutes."
+    MINS_INTERVAL="*/20"
+elif (( MINS_INTERVAL < 59 )) && (( MINS_INTERVAL > 0 )); then
+    echo "Adding job to run every ${MINS_INTERVAL} hours."
+    MINS_INTERVAL="*/${MINS_INTERVAL}"
     echo "job_added" > $JOB_FILE
 else
-    echo "HOURS_DELAY must be 1-23."
+    echo "MINS_INTERVAL must be 1-59."
+    exit 1
+fi
+if [ -z "$HOURS_INTERVAL" ]; then
+    echo "HOURS_INTERVAL not set, defaulting to 1 hours."
+    HOURS_INTERVAL="*"
+elif (( HOURS_INTERVAL < 24 )) && (( HOURS_INTERVAL > 0 )); then
+    echo "Adding job to run every ${HOURS_INTERVAL} hours."
+    HOURS_INTERVAL="*/${HOURS_INTERVAL}"
+    echo "job_added" > $JOB_FILE
+else
+    echo "HOURS_INTERVAL must be 1-23."
     exit 1
 fi
 
-echo -e "0 ${HOURS_DELAY} * * * /bin/update-freedns.sh\n" > /var/spool/cron/crontabs/root
+echo -e "${MINS_INTERVAL} ${HOURS_INTERVAL} * * * /bin/update-freedns.sh\n" > /var/spool/cron/crontabs/root
